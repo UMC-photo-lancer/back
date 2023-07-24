1e1be256680807ff35597934405e5777ebe7bf10
임채연 <dlacodus0407@gmail.com>
2023-07-24 17:24:26 +0900

    Feat : /bookmark/{bookmarkName} 구현

--- src/main/java/shop/photolancer/photolancer/repository/PostBookmarkRepository.java
+++ src/main/java/shop/photolancer/photolancer/repository/PostBookmarkRepository.java
@@ -1,7 +1,11 @@
 package shop.photolancer.photolancer.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import shop.photolancer.photolancer.domain.Post;
 import shop.photolancer.photolancer.domain.mapping.PostBookmark;
 
 import java.util.List;
@@ -9,4 +13,7 @@ import java.util.List;
 public interface PostBookmarkRepository extends JpaRepository<PostBookmark, Long> {
     @Query("SELECT pb FROM PostBookmark pb JOIN FETCH pb.bookmark b WHERE pb.post.id = :postId")
     List<PostBookmark> findByPostIdWithBookmark(Long postId);
+
+    @Query("SELECT pb.post FROM PostBookmark pb JOIN pb.bookmark b ON pb.bookmark.id = b.id WHERE b.name = :postBookmarkName")
+    Page<Post> findByPostBookmarkName(Pageable pageable, @Param("postBookmarkName") String postBookmarkName);
 }
