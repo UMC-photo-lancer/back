f3ffa9cc0647b596159b1d732cf393265dc0daef
임채연 <dlacodus0407@gmail.com>
2023-08-06 18:52:53 +0900

    Update : 공지사항에 isAdmin 추가

--- src/main/java/shop/photolancer/photolancer/web/dto/NoticeResponseDto.java
+++ src/main/java/shop/photolancer/photolancer/web/dto/NoticeResponseDto.java
@@ -2,6 +2,7 @@ package shop.photolancer.photolancer.web.dto;
 
 import lombok.Builder;
 import lombok.Getter;
+import org.springframework.data.domain.Page;
 import shop.photolancer.photolancer.domain.enums.Category;
 
 public class NoticeResponseDto {
@@ -12,5 +13,12 @@ public class NoticeResponseDto {
         private Category category;
         private  String title;
         private String createdAt;
+    }
+
+    @Getter
+    @Builder
+    public static class NoticeListDto {
+        Page<NoticePagingDto> NoticePagingDto;
+        Boolean isAdmin;
     }
 }
