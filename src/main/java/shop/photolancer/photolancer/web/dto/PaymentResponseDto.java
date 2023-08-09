87107b096dfc10e0a2903819f4c648c29270455b
qkrwodbsfjq <qkrwodbsfjq@khu.ac.kr>
2023-08-10 05:39:17 +0900

    Update: Modify GET /my-profile/exchange Response

--- src/main/java/shop/photolancer/photolancer/web/dto/PaymentResponseDto.java
+++ src/main/java/shop/photolancer/photolancer/web/dto/PaymentResponseDto.java
@@ -39,4 +39,26 @@ public class PaymentResponseDto {
         private Integer userPoint;
     }
 
+    @Builder
+    @Getter
+    @AllArgsConstructor(access = AccessLevel.PROTECTED)
+    @NoArgsConstructor(access = AccessLevel.PROTECTED)
+    public static class PaymentUserDto{
+        private Long userId;
+        private Integer point;
+    }
+
+    @Getter
+    @Setter
+    @Builder
+    public static class UserInfoAndAccounts {
+        private PaymentUserDto userInfo;
+        private List<PaymentResponseDto.ExchangeDto> accounts;
+
+        public UserInfoAndAccounts(PaymentUserDto userInfo, List<PaymentResponseDto.ExchangeDto> accounts) {
+            this.userInfo = userInfo;
+            this.accounts = accounts;
+        }
+    }
+
 }
